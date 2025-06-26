b7cf0f9c78454513419463f81a013a89c448695d
Daniel-Juarez-7274 <daniel_alz_juarez.07@hotmail.com>
2025-06-26 01:47:56 -0600

    Correción de cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,19 @@
 import { CorsOptions } from 'cors'
 
+const whiteList = [
+    process.env.FRONTEND_URL,
+    'http://localhost:5173'
+].filter(Boolean);
+
 export const corsConfig: CorsOptions = {
-    origin: function(origin, callback) {
-
-        const whiteList = [process.env.FRONTEND_URL]
-
-        if(process.argv[2] === '--api') {
-            whiteList.push(undefined)
-        }
-        if(whiteList.includes(origin)) {
-            callback(null, true)
+    origin: function (origin, callback) {
+        
+        if (!origin || whiteList.includes(origin)) {
+            callback(null, true);
         } else {
-            callback(new Error('Error de CORS'))
+            console.error('CORS bloqueado para el origen:', origin);
+            callback(new Error('Error de CORS'));
         }
-    }
-}
+    },
+    credentials: true
+};
